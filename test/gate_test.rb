5ce6c59549e12d29bc72ff71d10f0e1b03632d37
Makotox0207 <xiaoxishicheng@gmail.com>
2021-08-22 03:30:36 +0900

    改札機プログラムのテストコードのリファクタリング

--- test/gate_test.rb
+++ test/gate_test.rb
@@ -3,23 +3,35 @@ require './lib/gate'
 require './lib/ticket'
 
 class GateTest < Minitest::Test
-  # 150円の切符を購入, 梅田で入場し十三で出場, 期待する結果:出場できる
-  def test_gate
-    umeda = Gate.new(:umeda)
-    juso = Gate.new(:juso)
+  def setup
+    @umeda = Gate.new(:umeda)
+    @juso = Gate.new(:juso)
+    @mikuni = Gate.new(:mikuni)
+  end
 
+  # 150円の切符を購入, 梅田で入場し十三で出場, 期待する結果:出場できる
+  def test_umeda_to_joso
     ticket = Ticket.new(150)
-    umeda.enter(ticket)
-    assert juso.exit(ticket)
+    @umeda.enter(ticket)
+    assert @juso.exit(ticket)
   end
 
   # 150円の切符を購入, 梅田で入場し三国で出場, 期待する結果:出場できない
   def test_umeda_to_mikuni_when_fare_is_not_enough
-    umeda = Gate.new(:umeda)
-    mikuni = Gate.new(:mikuni)
-
     ticket = Ticket.new(150)
-    umeda.enter(ticket)
-    refute mikuni.exit(ticket) # refute falseを期待する
+    @umeda.enter(ticket)
+    refute @mikuni.exit(ticket) # refute falseを期待する
+  end
+
+  def test_umeda_to_mikuni_when_fare_is_enough
+    ticket = Ticket.new(190)
+    @umeda.enter(ticket)
+    assert @mikuni.exit(ticket)
+  end
+
+  def test_juso_to_mikuni
+    ticket = Ticket.new(150)
+    @juso.enter(ticket)
+    assert @mikuni.exit(ticket)
   end
 end
